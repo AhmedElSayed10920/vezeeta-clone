747c778f51925ba6357563e33701a5871a110517
Mahmoud_Abdo <mahmoudibra798@gmail.com>
2025-04-06 13:24:03 +0200

    last edit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { BookingComponent } from '../booking/booking.component';
 import { DoctorsComponent } from '../doctors/doctors.component';
 import { OfferListComponent } from "../offer-list/offer-list.component";
 import { BookSpecialtiesComponent } from "../book-specialties/book-specialties.component";
+import { DownloadSectionComponent } from "../download-section/download-section.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, BookingComponent, DoctorsComponent, OfferListComponent, BookSpecialtiesComponent],
+  imports: [HeroComponent, BookingComponent, DoctorsComponent, OfferListComponent, BookSpecialtiesComponent, DownloadSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
