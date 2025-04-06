f9dab610107235d6659364b3d7f83035ce09dcfe
ahmed-elsayed-mohamed <Kaido10920@gmail.com>
2025-04-06 13:11:19 +0200

    tst2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { BookingComponent } from '../booking/booking.component';
 import { DoctorsComponent } from '../doctors/doctors.component';
-import { DoctorDetailsComponent } from '../doctor-details/doctor-details.component';
-import { AllDoctorsComponent } from '../all-doctors/all-doctors.component';
+import { DownloadSectionComponent } from "../download-section/download-section.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, BookingComponent,DoctorsComponent,DoctorDetailsComponent,AllDoctorsComponent],
+  imports: [HeroComponent, BookingComponent, DoctorsComponent, DownloadSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
