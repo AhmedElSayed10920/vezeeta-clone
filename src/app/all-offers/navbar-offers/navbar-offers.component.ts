665648275cf5c478c57c224a3c74ea7082a612a3
Mahmoud_Abdo <mahmoudibra798@gmail.com>
2025-04-07 23:05:04 +0200

    added navbar-offers

---
+++ src/app/all-offers/navbar-offers/navbar-offers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-offers',
+  imports: [],
+  templateUrl: './navbar-offers.component.html',
+  styleUrl: './navbar-offers.component.css'
+})
+export class NavbarOffersComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { AllOffersComponent } from './all-offers/all-offers.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LoginForDoctorComponent } from './login-for-doctor/login-for-doctor.component';
@@ -8,17 +9,18 @@ import { SignupComponent } from './signup/signup.component';
 import { AllDoctorsComponent } from './all-doctors/all-doctors.component';
 import { DoctorsComponent } from './doctors/doctors.component';
 import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
-
+import { SlidesComponent } from './all-offers/slides/slides.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'allOffers', component: AllOffersComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'loginForDoctor', component: LoginForDoctorComponent },
   { path: 'signupForDoctor', component: SignupForDoctorComponent },
   { path: 'contactUS', component: ContactUSComponent },
-  {path : 'allDoctors', component: AllDoctorsComponent},
-  {path : 'doctors', component: DoctorsComponent},
-  {path : 'doctorDetails/:id', component: DoctorDetailsComponent},
+  { path: 'allDoctors', component: AllDoctorsComponent },
+  { path: 'doctors', component: DoctorsComponent },
+  { path: 'doctorDetails/:id', component: DoctorDetailsComponent },
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
