458c99a975903fb0650d2ccae4a96d2727e3b4be
Mahmoud_Abdo <mahmoudibra798@gmail.com>
2025-04-11 20:32:16 +0200

    edit specialization4/11/2025 8:32pm

--- src/app/all-offers/navbar-offers/navbar-offers.component.ts
+++ src/app/all-offers/navbar-offers/navbar-offers.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-offers',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './navbar-offers.component.html',
   styleUrl: './navbar-offers.component.css'
 })
 export class NavbarOffersComponent {
 
-}
+
+
+
+  }
+  
+
+
+
