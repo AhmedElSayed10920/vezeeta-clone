20155ff7c5bf906aa973e91bccfa71cc2bb2d400
hasnaaabdo <hasnaawarda444@gmail.com>
2025-04-24 17:44:11 +0200

    popup update

--- src/app/medical-question/medical-question.component.ts
+++ src/app/medical-question/medical-question.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChatAssistantComponent } from '../chat-assistant/chat-assistant.component';
+import { CommonModule } from '@angular/common';
+import { ChatAIComponent } from '../components/chat-ai/chat-ai.component';
 
 @Component({
   selector: 'app-medical-question',
-  imports: [],
+  standalone: true,
+  imports: [ChatAssistantComponent,CommonModule,ChatAIComponent],
   templateUrl: './medical-question.component.html',
-  styleUrl: './medical-question.component.css'
+  styleUrls: ['./medical-question.component.css']
 })
 export class MedicalQuestionComponent {
-  constructor(private router: Router) { };
-  goToChatAssistant() {
-    this.router.navigate(['/chatAssistant']);
+  isChatOpen = false;
+  isChatAiOpen = false;
+
+  toggleChat() {
+    this.isChatOpen = !this.isChatOpen;
   }
+  toggleChatAi() {
+    this.isChatAiOpen = !this.isChatAiOpen;
+  }
+ 
+
 }
